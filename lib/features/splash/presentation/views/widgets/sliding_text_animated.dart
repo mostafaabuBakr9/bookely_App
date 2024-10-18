49f042693a0d8ddf9523aed09a30d619ae654412
AbuBakr14 <mostafaabubakr9@gmail.com>
2024-10-19 01:40:40 +0300

    finish splash screen with animations

---
+++ lib/features/splash/presentation/views/widgets/sliding_text_animated.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SlidingTextAnimated extends StatelessWidget {
+  const SlidingTextAnimated({
+    super.key,
+    required this.slidingAnimation,
+  });
+
+  final Animation<Offset> slidingAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: slidingAnimation,
+      builder: (context, _) {
+        return SlideTransition(
+          position: slidingAnimation,
+          child: Text(
+            'Read Free Books',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 35,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,34 @@
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/features/splash/presentation/views/widgets/sliding_text_animated.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 1), end: const Offset(0, 0))
+            .animate(animationController);
+    animationController.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,13 +37,7 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(Assets.splashLogo),
-        Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 35,
-          ),
-        )
+        SlidingTextAnimated(slidingAnimation: slidingAnimation)
       ],
     );
   }
