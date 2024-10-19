cce06d68bc1cb23f141d8ff8cf7b1ea248c7ea0f
AbuBakr14 <mostafaabubakr9@gmail.com>
2024-10-19 16:18:55 +0300

    refactor splach screen & navigate to hame view

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:bookly/features/splash/presentation/views/widgets/sliding_text_animated.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -15,12 +17,8 @@ class _SplashViewBodyState extends State<SplashViewBody>
   late Animation<Offset> slidingAnimation;
   @override
   void initState() {
-    animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 1));
-    slidingAnimation =
-        Tween<Offset>(begin: const Offset(0, 1), end: const Offset(0, 0))
-            .animate(animationController);
-    animationController.forward();
+    initslidingAnimation();
+    NavigateToHome();
     super.initState();
   }
 
@@ -40,5 +38,20 @@ class _SplashViewBodyState extends State<SplashViewBody>
         SlidingTextAnimated(slidingAnimation: slidingAnimation)
       ],
     );
+  }
+
+  void initslidingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 1), end: const Offset(0, 0))
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  void NavigateToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(() => const HomeView(), transition: Transition.fadeIn);
+    });
   }
 }
