2d095a519f4cbd04246d5fb2bd4d91c33d899161
AbuBakr14 <mostafaabubakr9@gmail.com>
2024-10-20 18:39:15 +0300

    Book detalis view done

--- lib/features/home/presentation/views/widgets/Book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/Book_details_view_body.dart
@@ -1,33 +1,57 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/Book_details_info.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_button.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar_book_details.dart';
+import 'future_Books_list_view.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const CustomAppBarBookDetails(),
-          SizedBox(
-              height: MediaQuery.sizeOf(context).height * 0.4,
-              child: CustomListViewItem()),
-          SizedBox(
-            height: 40,
-          ),
-          BookDetailsInfo(),
-          SizedBox(
-            height: 40,
-          ),
-          CustomButton(),
-        ],
-      ),
-    );
+    return CustomScrollView(slivers: [
+      SliverFillRemaining(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const CustomAppBarBookDetails(),
+            SizedBox(
+                height: MediaQuery.sizeOf(context).height * 0.4,
+                child: const CustomListViewItem()),
+            const SizedBox(
+              height: 40,
+            ),
+            const BookDetailsInfo(),
+            const SizedBox(
+              height: 40,
+            ),
+            const CustomButton(),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'You can also Like',
+                  style: Styles.googletextStyle14semiBold,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: MediaQuery.sizeOf(context).height * 0.18,
+              child: const FutureBooksListView(),
+            )
+          ],
+        ),
+      )
+    ]);
   }
 }
